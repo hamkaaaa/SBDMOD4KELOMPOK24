344f803bfa8b13b0e8818a7755319580cb9c24ae
hamkaaaa <hamkamuhammad5@gmail.com>
2026-04-27 16:29:48 +0700

    Update loanController.js

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -5,22 +5,20 @@ export const LoanController = {
     const { book_id, member_id, due_date } = req.body;
     try {
       const loan = await LoanModel.createLoan(book_id, member_id, due_date);
-      res.status(201).json({
-        message: "Peminjaman berhasil dicatat!",
-        data: loan
-      });
-    } catch (err) {
-      // Jika stok habis atau ID salah, kirim status 400 (Bad Request)
-      res.status(400).json({ error: err.message });
-    }
+      res.status(201).json({ message: "Peminjaman berhasil dicatat!", data: loan });
+    } catch (err) { res.status(400).json({ error: err.message }); }
   },
-
   async getLoans(req, res) {
     try {
       const loans = await LoanModel.getAllLoans();
       res.json(loans);
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
+    } catch (err) { res.status(500).json({ error: err.message }); }
+  },
+  async returnBook(req, res) {
+    try {
+      const { id } = req.params;
+      const result = await LoanModel.returnLoan(id);
+      res.json(result);
+    } catch (err) { res.status(400).json({ error: err.message }); }
   }
 };
